20f0948d91e063258fcb3e4bfe01f6ebc4c1f0c5
Patrique Ouimet <patrique.ouimet@gmail.com>
2024-03-02 16:04:23 -0500

    chore: attempt 4

--- index.php
+++ index.php
@@ -14,56 +14,67 @@ declare(strict_types=1);
  */
 $stations = [];
 
-function lines($file): Generator
-{
-    $f = fopen($file, 'r');
-    try {
-        while ($line = fscanf($f, '%[^;];%f')) {
-            yield $line;
-        }
-    } finally {
-        fclose($f);
-    }
-}
-
 $c = 0;
 
 $input = __DIR__.'/measurements.txt';
-// $input = __DIR__.'/top1000000.txt';
-foreach (lines($input) as $measurement) {
-    [$name, $temperature] = $measurement;
+// $input = __DIR__ . "/top1000000.txt";
 
-    if (!isset($stations[$name])) {
-        $stations[$name] = [
-            'count' => 0,
-            'total' => 0,
-            // max value of 99.99
-            'min' => 100,
-            // min value of -99.99
-            'max' => -100,
-        ];
+$f = fopen($input, 'r');
+$pos = 0;
+
+while (!feof($f)) {
+    fseek($f, $pos);
+
+    $data = fread($f, 1048576);
+
+    $last_pos = strrpos($data, "\n");
+
+    if ($last_pos === false) {
+        $last_pos = strlen($data);
     }
 
-    ++$stations[$name]['count'];
-    $stations[$name]['total'] += (float) $temperature;
+    $parsed = preg_split('/[\n;]/', substr($data, 0, $last_pos));
+    for ($i = 0; $i < count($parsed); $i += 2) {
+        if ($parsed[$i] === '') {
+            ++$i;
+        }
 
-    if ($temperature < $stations[$name]['min']) {
-        $stations[$name]['min'] = (float) $temperature;
+        $name = $parsed[$i];
+        $temperature = (float) $parsed[$i + 1];
+
+        if (!isset($stations[$name])) {
+            $stations[$name] = [
+                'count' => 0,
+                'total' => 0,
+                // max value of 99.99
+                'min' => 100,
+                // min value of -99.99
+                'max' => -100,
+            ];
+        }
+
+        ++$stations[$name]['count'];
+        $stations[$name]['total'] += $temperature;
+
+        if ($temperature < $stations[$name]['min']) {
+            $stations[$name]['min'] = $temperature;
+        }
+
+        if ($temperature > $stations[$name]['max']) {
+            $stations[$name]['max'] = $temperature;
+        }
+
+        ++$c;
+
+        if ($c % 10_000_000 === 0) {
+            echo $c . PHP_EOL;
+        }
     }
 
-    if ($temperature > $stations[$name]['max']) {
-        $stations[$name]['max'] = (float) $temperature;
-    }
-
-    ++$c;
-
-    if ($c % 10_000_000 === 0) {
-        echo $c . PHP_EOL;
-    }
+    $pos += $last_pos;
 }
 
-$filename = __DIR__.'/output.txt';
-file_put_contents($filename, '{');
+$filename = __DIR__ . '/output.txt';
 
 // sort by station name
 ksort($stations);
@@ -71,14 +82,18 @@ ksort($stations);
 $last = array_key_last($stations);
 
 $station_count = 0;
-$output = '';
+$output = '{';
 
 foreach ($stations as $name => $data) {
-    $output .= $name . '='
-        . $data['min']
-        . '/' . number_format($data['total'] / $data['count'], 1)
-        . '/' . $data['max']
-        . ($name === $last ? '' : ', ');
+    $output .=
+        $name .
+        '=' .
+        $data['min'] .
+        '/' .
+        number_format($data['total'] / $data['count'], 1) .
+        '/' .
+        $data['max'] .
+        ($name === $last ? '' : ', ');
 
     ++$station_count;
 
